1d464385d07cbfbe8e34e77d4d690193a4b89ed1
Khushal Sahni <kstheking0@gmail.com>
2018-12-04 12:57:00 +0530

    updating colors

---
+++ src/components/Start learning/colors/AppColors.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Navbar from '../../Navbar.js';
+import Header from './ColorsHeader.js';
+
+import './colors.scss';
+
+class AppColors extends React.Component {
+	render() {
+		return (
+			<div>
+				<Navbar />
+				<Header />
+			 {/* <Menu /> */}
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/components/Start learning/colors/colors.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppColors from './AppColors.js';
+import './colors.scss';
+
+ReactDOM.render(<AppColors />, document.getElementById('rainbow'));
