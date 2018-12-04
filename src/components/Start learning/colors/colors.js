e1895236c0e5769d83135d0f854068f17bfb1b7c
Khushal Sahni <kstheking0@gmail.com>
2018-12-04 13:15:04 +0530

    Updating colors

--- src/components/Start learning/colors/colors.js
+++ src/components/Start learning/colors/colors.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppColors from './AppColors.js';
+import AppColors from './AppColors';
+import * as serviceWorker from '../../serviceWorker';
 import './colors.scss';
 
 ReactDOM.render(<AppColors />, document.getElementById('rainbow'));
