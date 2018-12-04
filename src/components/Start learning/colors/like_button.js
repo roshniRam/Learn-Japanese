9ac58e6c7df8725991b82166572e80406034296f
Khushal Sahni <kstheking0@gmail.com>
2018-12-04 06:57:40 +0530

    updating

--- src/components/Start learning/colors/like_button.js
+++ src/components/Start learning/colors/like_button.js
@@ -20,5 +20,4 @@ class LikeButton extends React.Component {
     );
   }
 }
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(e(LikeButton), document.getElementById('rainbow'));
