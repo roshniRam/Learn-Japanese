34317accc0b29d635225ae80e4913c95d6a91f13
Khushal Sahni <kstheking0@gmail.com>
2018-12-05 17:23:44 +0530

    Fixing

--- src/colors.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppColors from './AppColors';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<AppColors />, document.getElementById('rainbow'));
-serviceWorker.unregister();
