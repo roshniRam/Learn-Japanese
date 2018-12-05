fb763c001de53d64071d1de6a07e3770cbb9c753
Khushal Sahni <kstheking0@gmail.com>
2018-12-05 18:45:42 +0530

    Updating

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import Colors from './colors/colors'
 import Navbar from './Navbar';
 import Header from './Header';
 import Menu from './Menu';
-
+import {BrowserRouter, Route} from 'react-router-dom';
 import '../sass/main.scss';
 
 class App extends React.Component {
 	render() {
 		return (
+			<BrowserRouter>
+			<div>
+			<Route exact={true} path='/' render={() => (
 			<div>
 				<Navbar />
 				<Header />
 			 {/* <Menu /> */}
 			</div>
+			)}/>
+			<Route exact={true} path='/colors' render={() => (
+			<div>
+				<Colors />
+			</div>
+			)}/>
+			</div>
+			</BrowserRouter>
 		);
 	}
 }
---
+++ src/components/Start learning/colors/colors.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../../sass/main.scss';
+import Navbar from '../Navbar';
+export default class Colors extends Component {
+  state = {
+  }
+
+  render () {
+      return (
+        <Navbar />
+      )
+   }
+}
