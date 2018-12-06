b53276afc3bbbf2bb928e31419ad6faea2d3b4ed
Khushal Sahni <kstheking0@gmail.com>
2018-12-06 13:58:21 +0530

    Updating colors page

--- src/components/Start learning/colors/colors.js
+++ src/components/Start learning/colors/colors.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import '../../../sass/main.scss';
 import Navbar from '../../Navbar';
+import ColorsHeader from './colorsHeader';
 export default class Colors extends Component {
   state = {
   }
 
   render () {
       return (
+        <div>
         <Navbar />
+        <ColorsHeader />
+        </div>
       )
    }
 }
